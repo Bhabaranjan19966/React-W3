11874880574970638b5258e7574e5f95752b7772
Bhabaranjan19966 <bhabaranjanpanigrahi@gmail.com>
2018-09-17 17:16:43 +0530

    webpack4 configured for react and css

---
+++ src/App/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import{Component } from 'react';
+import './style.css';
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+            <h1>Hello</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
